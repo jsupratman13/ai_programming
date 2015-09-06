d2c9a0515a8feb5484442cad29a38c3f944b94a2
jsupratman13 <s1426071BL@s.chibakoudai.jp>
2015-09-06 17:43:47 +0900

    update program

--- testlist.py
+++ testlist.py
@@ -1,7 +1,26 @@
 import sys
-import simplegoap
+from simplegoap import List, Action
+
 def example():
-	pass
+	model = List(['at_armory', 'empty_backpack', 'empty_hands'])
+	print 'Initial Model: ',
+	model.PrintList()
+	
+	action = Action('pickup_spear', 
+			['at_armory','empty_hands'], 
+			['hold_spear'],
+			['empty_hands'])
+	model.UpdateList(action)
+	print 'Updated Model: ',
+	model.PrintList()
+
+	action = Action('dropoff_spear', 
+			['at_armory', 'hold_spear'], 
+			['empty_hands'], 
+			['hold_spear'])
+	model.UpdateList(action)
+	print 'Updated Model: ',
+	model.PrintList()
 
 
 if __name__ == '__main__':
