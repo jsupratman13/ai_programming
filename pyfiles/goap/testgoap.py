983c0dedfd21eacfe30eb8a7da599f1f7bdbae3c
jsupratman13 <s1426071BL@s.chibakoudai.jp>
2015-09-25 16:36:05 +0900

    update main

--- pyfiles/goap/testgoap.py
+++ pyfiles/goap/testgoap.py
@@ -1,14 +1,37 @@
-import math, sys, traceback
+import math, sys, traceback, time
 from goap import World, ActionList, Planner
 
 def main():
-	pass
+	model = World([])
+	goals = model.GoalList(['ball_in_target'])
+	initial_list = []
+
+	actions = []
+	#Name, precondition, addlist, dellist, cost
+	actions.append(ActionList('Localize',[],['know_self_pos'],[],2))
+	actions.append(ActionList('SearchBall',[],['know_ball_pos'],[],3))
+	actions.append(ActionList('ApproachBall', ['know_ball_pos'], ['have_ball'],[], 5))
+	actions.append(ActionList('TurnAroundBall', ['know_self_pos', 'have_ball'],['ball_straight'], ['ball_kickarea'], 5))
+	actions.append(ActionList('AdjustToKick', ['have_ball', 'ball_straight'], ['ball_kickarea'], [], 8))
+	actions.append(ActionList('Shoot', ['ball_kickarea', 'ball_straight', 'have_ball'], ['ball_in_target'], ['ball_kickarea', 'ball_straight', 'have_ball'], 5))
+
+	task = Planner(initial_list, actions, goals)
+	plans = task.process()
+
+	for plan in plans:
+		print plan,
+	print ''
+
 
 if __name__ == '__main__':
 	try:
+		initial_time = time.time()
 		main()
+		print 'time: ' + str(initial_time - time.time())
+
 	except Exception, e:
 		print traceback.format_exc()
 		print 'Exception: '+str(e)
+
 	except KeyboardInterrupt:
 		sys.exit()
