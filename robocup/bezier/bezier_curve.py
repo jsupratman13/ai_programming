00481ec64792b0571ff890c82930f912c6786ce2
jsupratman13 <s1426071BL@s.chibakoudai.jp>
2015-10-31 22:07:41 +0900

    added bezier curve

--- robocup/bezier/bezier_curve.py
+++ robocup/bezier/bezier_curve.py
@@ -1,32 +1,36 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-#TODO: save control point
-def quadraticBezier(p0, p1, p2, p3, t):
-	x0 ,y0, z0 = p0
-	x1, y1, z1 = p1
-	x2, y2, z2 = p2
-	x3, y3, z3 = p3
-
-	xf = math.pow(1-t,2)*x0+(1-t)*2*t*x1+t*t*x2
-	yf = math.pow(1-t,2)*y0+(1-t)*2*t*y1+t*t*y2
-	zf = 0
-
-	return (xf, yf, zf) 
-
-#TODO: save control point
-def cubicBezier(p0, p1, p2, p3, t):
-	x0 ,y0, z0 = p0
-	x1, y1, z1 = p1
-	x2, y2, z2 = p2
-	x3, y3, z3 = p3
+def quadraticBezier(ps, pc, pt):
+	x0 ,y0 = ps #start point
+	x1, y1 = pc #control point
+	x2, y2 = pt #target point
 	
-	xf = math.pow(1-t,3)*x0+math.pow(1-t,2)*3*t*x1+(1-t)*3*t*t*x2+t*t*t*x3
-	yf = math.pow(1-t,3)*y0+math.pow(1-t,2)*3*t*y1+(1-t)*3*t*t*y2+t*t*t*y3
-	zf = 0
+	xf = []
+	yf = []
 
-	return(xf, yf, zf)
+	for t in np.linspace(0,1): #0<t<1
+		xf.append(math.pow(1-t,2)*x0+(1-t)*2*t*x1+t*t*x2)
+		yf.append(math.pow(1-t,2)*y0+(1-t)*2*t*y1+t*t*y2)
+
+	return xf, yf
+
+
+def cubicBezier(ps, pc1, pc2, pt):
+	x0 ,y0 = ps	 #start point
+	x1, y1 = pc1 #control point 1
+	x2, y2 = pc2 #control point 2
+	x3, y3 = pt  #target point
+
+	xf = []
+	yf = []
+	for t in np.linspace(0,1):
+		xf.append(math.pow(1-t,3)*x0+math.pow(1-t,2)*3*t*x1+(1-t)*3*t*t*x2+t*t*t*x3)
+		yf.append(math.pow(1-t,3)*y0+math.pow(1-t,2)*3*t*y1+(1-t)*3*t*t*y2+t*t*t*y3)
+
+	return xf, yf
 
 def passthroughpoint(p0, p1, p2): #Return control point necessary to pass through point p
 	x0 ,y0, z0 = p0
@@ -35,3 +39,25 @@ def passthroughpoint(p0, p1, p2): #Return control point necessary to pass throug
 	
 	cpx = x1 * 2 - (x0 + x2) / 2
 	cpy = y1 * 2 - (y0 + y2) / 2
+
+if __name__ == '__main__':
+	plt.xlim(0,100)
+	plt.ylim(0,100)
+	plt.grid(True)
+
+	ps = (10,10)
+	pt = (90, 50)
+	pc = (50,90)
+	
+	x,y = quadraticBezier(ps, pc, pt)
+	x2, y2 = cubicBezier(ps,pc, (70,30), pt)
+
+	plt.plot(x,y, 'bo')
+	plt.plot(x2, y2, 'go')
+
+	plt.plot(10,10, 'ro')
+	plt.plot(90,50, 'ro')
+	plt.plot(50,90, 'ro')
+	plt.plot(70,30, 'ro')
+	plt.show()
+	
--- robocup/bezier/linear_interpolation.py
+++ robocup/bezier/linear_interpolation.py
@@ -1,3 +1,43 @@
 import numpy as np
-import scipy
+import matplotlib.pyplot as plt
+import scipy.interpolate as itp
 
+def test():
+	x,y = random_coordinate(3)
+
+	plt.plot(x,y, 'ro')
+	print x,y	
+	
+	#baycentric langrange
+	interpo1 = itp.BarycentricInterpolator(x,y) 
+	#Krogh
+	#interpo2 = itp.KroghInterpolator(x,y)
+	xnew = np.arange(-20,20, 0.1)
+	ynew = interpo1(xnew)
+
+	plt.plot(xnew, ynew, 'b-')
+
+def random_coordinate(coordinate):
+	datax = []
+	datay = []
+	for n in range(0,coordinate):
+		datax.append(np.random.random_integers(1,10))
+		datay.append(np.random.random_integers(1,10))
+	return datax, datay
+
+def plotfunc():
+	plt.xlim(0,10)
+	plt.ylim(0,100)
+	plt.grid(True)
+
+	x = np.linspace(0.0, 10.0)
+	y = x**2
+
+	plt.plot(x,y, 'ro')
+
+if __name__ == '__main__':
+	plt.xlim(-20,20)
+	plt.ylim(-20,20)
+	plt.grid(True)
+	test()
+	plt.show()
