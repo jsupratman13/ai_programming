ed2218e8c90a0d21599ce27c28307f180e520f0c
jsupratman13 <s1426071BL@s.chibakoudai.jp>
2015-11-01 21:54:40 +0900

    completed bezier curve

--- robocup/bezier/bezier_curve.py
+++ robocup/bezier/bezier_curve.py
@@ -2,8 +2,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def quadraticBezier(ps, pc, pt):
+def quadraticBezier(ps, pt, pc):
 	x0 ,y0 = ps #start point
 	x1, y1 = pc #control point
 	x2, y2 = pt #target point
@@ -18,8 +17,8 @@ def quadraticBezier(ps, pc, pt):
 	return xf, yf
 
 
-def cubicBezier(ps, pc1, pc2, pt):
-	x0 ,y0 = ps	 #start point
+def cubicBezier(ps, pt, pc1, pc2):
+	x0 ,y0 = ps  #start point
 	x1, y1 = pc1 #control point 1
 	x2, y2 = pc2 #control point 2
 	x3, y3 = pt  #target point
@@ -32,7 +31,41 @@ def cubicBezier(ps, pc1, pc2, pt):
 
 	return xf, yf
 
-def passthroughpoint(ps, pc, pt): #new control point necessary to pass through point pc
+def BezierCurve(ps, pt, *args):
+	if len(args) == 1:
+		pc = args[0]
+		return quadraticBezier(ps,pt,pc)
+	if len(args) == 2:
+		pc1 = args[0]
+		pc2 = args[1]
+		return cubicBezier(ps, pt, pc1, pc2)
+	xf = []
+	yf = []
+	n = len(args)
+	for i in range(0,n-1):
+		p0 = args[i]
+		p1 = args[i+1]
+		x,y = quadraticBezier(ps, midpoint(p0, p1), p0)
+		for xi in x:
+			xf.append(xi)
+		for yi in y:
+			yf.append(yi)
+		ps = midpoint(p0, p1)
+	x,y = quadraticBezier(ps, pt, args[n-1])
+	for xi in x:
+		xf.append(xi)
+	for yi in y:
+		yf.append(yi)
+	return xf, yf
+
+def midpoint(p1, p2):
+	x1, y1 = p1
+	x2, y2 = p2
+	midx = (x1 + x2) / 2
+	midy = (y1 + y2) / 2
+	return midx, midy
+
+def passthroughpoint(ps, pt, pc): #new control point necessary to pass through point pc
 	x0 ,y0 = ps	#start point
 	x1, y1 = pc #pass through point
 	x2, y2 = pt #target point
@@ -50,19 +83,20 @@ if __name__ == '__main__':
 	pt = (90, 50)
 	pc = (50,90)
 	pc2 = (70,30)
-	cx, cy = passthroughpoint(ps, pc, pt)
+	cx, cy = passthroughpoint(ps, pt, pc)
 
-	x,y = quadraticBezier(ps, pc, pt)
-	x2, y2 = cubicBezier(ps, pc, pc2, pt)
-	x3, y3 = quadraticBezier(ps, (cx,cy), pt)
+#	x3, y3 = quadraticBezier(ps, pt ,(cx,cy))
+	x4, y4 = BezierCurve(ps,pt,pc, pc2)
+	x, y = BezierCurve(ps, pt, (30,70), (50, 30),(70,90))
 
-	plt.plot(x,y, 'bo')
-	plt.plot(x2, y2, 'go')
-	plt.plot(x3, y3, 'ko')
-
+	plt.plot(x,y ,'b-')
+#	plt.plot(x3, y3, 'ko')
+	plt.plot(x4, y4, 'r-')
+	print x
 	plt.plot(10,10, 'ro')
 	plt.plot(90,50, 'ro')
-	plt.plot(50,90, 'ro')
-	plt.plot(70,30, 'ro')
+	plt.plot(70,90, 'ro')
+	plt.plot(50,30, 'ro')
+	plt.plot(30,70, 'ro')
 	plt.show()
 	
