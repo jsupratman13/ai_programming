22d02610cc04586456f70a84eb35b67c87717293
jsupratman13 <s1426071BL@s.chibakoudai.jp>
2015-11-26 21:39:41 +0900

    added successor, update dict, find neighbor, condition met

--- pyfiles/goap/goap2.py
+++ pyfiles/goap/goap2.py
@@ -1,15 +1,15 @@
 class WorldState(object):
 	def __init__(self, *args):
 		self.define_status = args
-		self.initial_state = None
 		self.goal_state = None
+		self.current_state = None
 
 	def set_initialstate(self, **kwargs):
 		if len(kwargs) != len(self.define_status): assert False, "number of initial state does not match with WorldState"
 		for state in kwargs:
 			if state not in self.define_status:
 				assert False, "initial state does not match with WorldState"
-		self.initial_state = kwargs
+		self.current_state = kwargs
 	
 	def set_goalstate(self, **kwargs):
 		for state in kwargs:
@@ -36,7 +36,7 @@ class Planner(object):
 		self.actionlist = actionlist
 		self.check()
 		if planstyle == 'dfs':
-			self.pathplan = DepthFirstSearch(self.world, self,actionlist)
+			assert False, "DepthFirstSearch not created yet"
 		else:
 			self.pathplan = AstarSearch(self.world, self.actionlist)
 		
@@ -54,28 +54,95 @@ class Planner(object):
 			print action.name
 	
 	def print_world(self):
-		for status in self.world.initial_state.iteritems():
+		for status in self.world.current_state.iteritems():
 			print status,
 
 	def process(self):
 		print 'searching plan'
 		return self.pathplan.formulate()
 
+class PartialPlan(object):
+	def __init__(self,world, actionlist, calc_cost):
+		self.world = world
+		self.actionlist = actionlist
+		self.cost = calc_cost
+	
+	def print_plan(self):
+		for state in self.world.current_state.iteritems():
+			print state,
+		print ""
+		for action in self.actionlist:
+			print action.name,
+		print ""
+		print self.cost
+
 class AstarSearch(object):
 	def __init__(self, world, actionlist):
 		self.world = world
 		self.actionlist = actionlist
+		self.ol = []
+		self.cl = []
 	
-	def neighbor(self):
-		pass
+	def neighbor(self, parent): #TODO: remove repeatitive action
+		neighbor_list = []
+		for action in self.actionlist:
+			for precondition in action.precondition.iteritems():
+				print precondition[0]
+				if precondition not in parent.current_state.iteritems():
+					print 'precon not match'
+					break
+			else:
+				neighbor_list.append(action)
+				print 'precon match new neighbor'
+		return neighbor_list
+
+	def update_status(self, action, parent): #TODO: copy classes to prevent change simultaneously
+		successor = parent
+		successor.world.current_state.update(action.effects)
+		for suc in successor.world.current_state.iteritems():
+			print suc,
+		successor.cost = successor.cost + action.cost #TODO; heuristic calculation and cost adjustment
+
+		return successor
+
+	def condition_met(self, successor):
+		for goal in self.world.goal_state.iteritems():
+			if goal not in successor.world.current_state.iteritems():
+				return False
+		return True
 
 	def formulate(self):
-		pass
+		successor = PartialPlan(self.world, [],0)
+		self.ol.append(successor)
+		parent = self.ol.pop(0)
+		successor_list = self.neighbor(parent.world)
+		for action in successor_list:
+			successor = self.update_status(action, parent)
+#		while self.ol:
+			#TODO: sort ollist in order of least successor cost to greatest
+#			parent = self.ol.pop(0)
+#			successor_list = self.neigbor(parent.world)
+#			for action in successor_list:
+#				successor = upadate_status(action, parent)
+#				if self.condition_met(successor):
+#					return successor.actionlist
+					
+			
+			
+			
+		
 
-class DepthFirstSearch(object):
-	def __init__(self, world, actionlist):
-		self.world = world
-		self.actionlist = actionlist
+if __name__ == '__main__':
+	world = WorldState('eat','sleep')
+	world.set_initialstate(eat=False,sleep=False)
+	world.set_goalstate(sleep=True)
 	
-	def formulate(self):
-		pass
+	alist = []
+
+	action = Action('get_food', 1)
+	action.set_precondition(eat=False,sleep=False)
+	action.set_effects(eat=True)
+	alist.append(action)
+
+	task = Planner(world,alist)
+	task.process()
