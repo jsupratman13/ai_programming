0a5c22324b15119cb3a79922f97a0aa9e198838e
jsupratman13 <s1426071BL@s.chibakoudai.jp>
2016-06-08 18:45:16 +0900

    edit bt cpp

---
+++ cppfiles/bt/ex2.cpp
@@ -0,0 +1,14 @@
+//www.cplusplus.com/forum/general/141582
+//Behaviour Tree
+
+#include <iostream>
+#include <list>
+#include <vector>
+#include <stack>
+#include <initializer_list>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+
+class BehaviorTree{
