03dc2b8fbd59c201ddcf1e189a098b3769d7616d
jsupratman13 <s1426071BL@s.chibakoudai.jp>
2016-06-15 14:53:44 +0900

    read text file from numpy

--- dp.py
+++ dp.py
@@ -28,14 +28,29 @@ class FileSystem(object):
 			print >> sys.stderr, 'cannot open "%s"' %filename
 			sys.exit(1)
 
+	def np_read(self, folder, num):
+		num = '%003d' % num
+		filename = str(folder)+'/'+str(folder)+'_'+str(num)+'.txt'	
+		try:
+			with open(filename, 'r') as f:
+				f.seek(0)
+				self.name = f.readline().rstrip()
+				self.word = f.readline().rstrip()
+				self.frame_length = int(f.readline())
+				f.close()
+			self.data = np.loadtxt(filename, skiprows=3, usecols=range(15))
+		except IOError:
+			print >> sys.stderr, 'cannot open "%S"' %filename
+			sys.exit(1)
+
 class DPMatching(object):
 	def __init__(self, template, unknown):
 		self.template = copy.deepcopy(template)
 		self.unknown = copy.deepcopy(unknown)
 		self.i = self.template.frame_length
 		self.j = self.unknown.frame_length
-		self.d = np.empty([self.i, self.j])*0
-		self.g = np.empty([self.i, self.j])*0
+		self.d = np.zeros([self.i, self.j], dtype=np.float64)
+		self.g = np.zeros([self.i, self.j], dtype=np.float64)
 
 	def get_weight(self):
 		self.local_dist()
@@ -49,14 +64,15 @@ class DPMatching(object):
 		for i in range(self.i):
 			for j in range(self.j):
 				for k in range(15):
-					self.d[i,j] += math.pow(u[i,k]-t[j,k],2)
-				self.d[i,j] = math.sqrt(self.d[i,j])
+					self.d[i,j] += math.pow(t[i,k]-u[j,k],2)
+				self.d[i,j] = math.sqrt(math.fabs(self.d[i,j]))
 	
 	def algorithm(self):
 		for i in range(self.i):
 			for j in range(self.j):
 				if i==0 and j==0:
 					self.g[0,0] = self.d[0,0]
+					#self.g[0,0] = 0
 				elif i == 0:
 					self.g[i,j] = self.g[i,j-1] + self.d[i,j]
 				elif j == 0:
@@ -72,8 +88,8 @@ def dp_match(template, unknown):
 	u = unknown.data
 	J = template.frame_length
 	I = unknown.frame_length
-	d = np.empty([140,140])*0
-	g = np.empty([140,140])*0
+	d = np.zeros([140,140])
+	g = np.zeros([140,140])
 	for i in range(I):
 		for j in range(J):
 			for k in range(15):
@@ -102,12 +118,14 @@ if __name__ == '__main__':
 	count=0
 	initial_time = time.time()
 	for i in range(100):
-		unknown.read_para(UNKNOWN,i+1)
+#		unknown.read_para(UNKNOWN,i+1)
+		unknown.np_read(UNKNOWN, i+1)
 		for j in range(100):
-			template.read_para(TEMPLATE, j+1)
-#			dp = DPMatching(template, unknown)
-#			cost = dp.get_weight()
-			cost = dp_match(template, unknown)
+#			template.read_para(TEMPLATE, j+1)
+			template.np_read(TEMPLATE, i+1)
+			dp = DPMatching(template, unknown)
+			cost = dp.get_weight()
+#			cost = dp_match(template, unknown)
 			if j==0 or minimum_cost > cost:
 				minimum_cost = cost
 				print 'template: '+str(template.name)+ ' ' + str(cost)
