deee19b9e809f036365de54692e61cf9c48afebf
jsupratman13 <s1426071BL@s.chibakoudai.jp>
2016-06-18 12:23:20 +0900

    move to workspace repository

--- cppfiles/exercise3.cpp
+++
@@ -1,9 +0,0 @@
-//exercise3: using windows header file
-#include <windows.h>
-#include "wiiuse.h"
-
-int WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
-{
-	MessageBox(0, "Hello, Windows", "MinGW Test Program", MB_OK);
-	return 0;
-}
