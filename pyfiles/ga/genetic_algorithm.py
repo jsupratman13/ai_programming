108df38a8cfbcf4cb334af07f160b16314ad7d60
jsupratman13 <s1426071BL@s.chibakoudai.jp>
2016-12-14 17:51:06 +0900

    added example program from @lethain

---
+++ pyfiles/ga/genetic_algorithm.py
@@ -0,0 +1,69 @@
+from random import randint, random
+from operator import add
+
+def create_chromosome(length, min_val,max_val):
+    #length: number of gene in chromosome
+    #max_val,min_val: randomize allele within these value
+    return [randint(min_val,max_val) for x in xrange(length)]
+
+def create_population(count, length, min_val, max_val):
+    #count: number of chromosome in population
+    #length: number of gene in chromosome
+    #max_val,min_val: randomize allele within these value
+    return [create_chromosome(length, min_val, max_val) for x in xrange(count)]
+
+def fitness_function(chromosome, target):
+    total = reduce(add,chromosome,0)
+    #total = reduce(lambda x,y: x+y, chromosome,0)
+    return abs(target-total)
+
+def grade(population,target):
+    #find average fitness of population
+    avg = reduce(add,(fitness_function(chromosome,target) for chromosome in population))
+    return avg/(len(population)*1.0)
+
+def evolve(population, target, retain=0.2, random_select=0.5, mutate=0.01):
+    graded = [(fitness_function(chromosome, target), chromosome) for chromosome in population]
+    graded = [ x[1] for x in sorted(graded)]
+    retain_length = int(len(graded)*retain)
+    parents = graded[:retain_length]
+    #randomly add indviduals to promote genetic diversity
+    for individual in graded[retain_length:]:
+        if random_select > random():
+            parents.append(individual)
+    #mutate some individual: RANDOM RESETTING
+    for individual in parents:
+        if mutate > random():
+            position_to_mutate = randint(0,len(individual)-1)
+            individual[position_to_mutate] = randint(min(individual), max(individual))
+    #crossover: MULTIPOINT CROSSOVER
+    parents_length = len(parents)
+    desired_length = len(population) - parents_length
+    children = []
+    while len(children) < desired_length:
+        male = randint(0, parents_length-1)
+        female = randint(0, parents_length-1)
+        if male != female:  
+            male = parents[male]
+            female = parents[female]
+            half = len(male)/2
+            child = male[:half] + female[half:]
+            children.append(child)
+    parents.extend(children)
+    return parents
+
+if __name__ == '__main__':
+    target = 371
+    number_of_population = 100
+    length_of_chromosome = 5
+    min_length = 0
+    max_length = 100
+    p = create_population(number_of_population, length_of_chromosome, min_length, max_length)
+    fitness_history = [grade(p,target),]
+    for i in xrange(100):
+        p = evolve(p,target)
+        fitness_history.append(grade(p,target))
+    for datum in fitness_history:
+        print datum
+    print fitness_history
+
