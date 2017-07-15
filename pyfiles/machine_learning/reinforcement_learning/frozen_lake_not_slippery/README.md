f74f354b759c925715301af987f15f724ec6b56c
jsupratman13 <supratmanjoshua@gmail.com>
2017-07-15 19:42:55 +0900

    added deterministic frozen lake problem

---
+++ pyfiles/machine_learning/reinforcement_learning/frozen_lake_not_slippery/README.md
@@ -0,0 +1,36 @@
+# Frozen Lake Not Slippery problem
+Same thing as frozen lake but no noise, all action are deterministic
+
+## Training and Testing
+```
+python <filename>.py 
+```
+
+## Environment
+### Observation
+Type: Discrete
+
+Num | | | ||
+----|-|-|-|-
+_|0|1|2|3
+_|4|5|6|7
+_|8|9|10|11
+_|12|13|14|15
+
+Observation | | | | |
+------------|-|-|-|-
+_|S|F|F|F
+_|F|H|F|H
+_|F|F|F|H
+_|H|F|F|G
+
+### Actions
+Type: Discrete
+
+Num | Observation
+----|------------
+0   | move left
+1   | move down
+2   | move right
+3   | move up
+
---
+++ pyfiles/machine_learning/reinforcement_learning/frozen_lake_not_slippery/q_learning.py
@@ -0,0 +1,79 @@
+import gym
+from gym.envs.registration import register
+register(
+    id='FrozenLakeNotSlippery-v0',
+    entry_point='gym.envs.toy_text:FrozenLakeEnv',
+    kwargs={'map_name': '4x4', 'is_slippery':False},
+    reward_threshold=0.78 #optimum=0.8196
+)
+import numpy as np
+import sys
+
+class Agent(object):
+    def __init__(self, env):
+        self.env = env
+        self.gamma = 0.99
+        self.alpha = 0.01
+        self.num_episodes = 20000
+        self.num_trials = 100
+        self.epsilon = 0.3
+        self.Q = np.zeros([env.observation_space.n, env.action_space.n])
+
+    def save_table(self, filename='check.npy'):
+        np.save(filename, self.Q)
+    
+    def load_table(self, filename):
+        self.Q = np.load(filename)
+
+    def epsilon_greedy(self, Q, state):
+        e = np.random.uniform()
+        if e < self.epsilon:
+            action = self.env.action_space.sample()
+        else:
+            action = np.argmax(Q[state,:])
+        return action
+
+    def train(self):
+        for episode in range(self.num_episodes):
+            s = self.env.reset()
+            while True:
+                a = self.epsilon_greedy(self.Q, s)
+                s2, r, done, info = self.env.step(a)
+                self.Q[s,a] = self.Q[s,a] + self.alpha * (r + self.gamma * np.max(self.Q[s2,:]) - self.Q[s,a])
+                s = s2
+                if done: break
+        self.save_table()
+
+    def test(self, render=False):
+        total_reward = 0
+        if render: self.num_trials = 1
+        for i in range(self.num_trials):
+            s = self.env.reset()
+            reward = 0
+            while True:
+                if render: self.env.render()
+                a = np.argmax(self.Q[s,:])
+                s2, r, done, info = self.env.step(a)
+                s = s2
+                reward += r
+                if done:
+                    break
+            total_reward += reward
+        print 'success rate: ' + str(total_reward/self.num_trials)
+
+if __name__=='__main__':
+    if len(sys.argv) < 2:
+        assert False, 'missing argument'
+    env = gym.make('FrozenLakeNotSlippery-v0')
+    agent = Agent(env)
+    if str(sys.argv[1]) == 'train':
+        if len(sys.argv) > 2: agent.load_table(str(sys.argv[2]))
+        agent.train()
+    if str(sys.argv[1]) == 'test':
+        if len(sys.argv) < 3: assert False, 'missng .npy table'
+        agent.load_table(str(sys.argv[2]))
+        if len(sys.argv) > 3 and str(sys.argv[3]) == 'render':
+               agent.test(render=True)
+        else:
+            agent.test()
+
